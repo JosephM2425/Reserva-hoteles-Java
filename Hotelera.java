5f523b236f8915cc9a10ac3e96355aad518a7ef9
Joseph Murillo <45979494+JosephM2425@users.noreply.github.com>
2020-12-17 09:12:52 -0600

    FINAL

---
+++ Hotelera.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelera;
+
+/**
+ *
+ * @author JosephPC
+ */
+public class Hotelera {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Frm_Menu.main(args);
+    }
+    
+}
